e767ff7de22f06a32f10070ebf8fe63a282553a4
DharanHaarish <hdharan@newengen.com>
2025-10-10 17:02:01 +0530

    improved preprocess and train script

--- src/train.py
+++ src/train.py
@@ -4,6 +4,7 @@ import logging
 import numpy as np
 import os
 import pickle
+import yaml
 
 if not os.path.exists("data/processed"):
     logging.error("processed data directory not found")
@@ -17,7 +18,23 @@ df_train = df_train.sort_values(by="date", ascending=True).reset_index(drop=True
 
 df_train = df_train.rename(columns={"date": "ds", "value": "y"})
 
-model = Prophet()
+with open("params.yaml", "r") as f:
+    params = yaml.safe_load(f)
+
+if params['train']['transform'].lower() == "log":
+    logging.info("Log transformation applied")
+    if params['train']['base'] == 10:
+        logging.info("Log base 10 transformation applied")
+        df_train['y'] = np.log10(df_train['y'])
+    elif params['train']['base'] == "e":
+        logging.info("Log base e transformation applied")
+        df_train['y'] = np.log(df_train['y'])
+else:
+    logging.info("No transformation applied")
+    pass
+
+hyperparameters = params['train']['hyperparameters']
+model = Prophet(**hyperparameters)
 model.fit(df_train)
 
 if not os.path.exists("models/"):
